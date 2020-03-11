4110ef86d6dfd86d96c9767a272417176747e7a4
maarcospaaulo <maarcospaaulo@live.com>
2020-03-11 15:04:24 -0300

    adicionando os svgs

--- js/stage.js
+++ js/stage.js
@@ -1,13 +1,13 @@
 $('.carousel-stage').slick({
-    dots: true, // exibe os pontos a baixo
-    infinite: false, // sempre volta ao inicio
+    dots: false, // exibe os pontos a baixo
+    infinite: true, // sempre volta ao inicio
     slidesToShow: 4, // qtdd de slides que serao exibidos
     slidesToScroll: 4, // qtdd de slides que serao movidos para o lado
     responsive: [
         {
             breakpoint: 768,
             settings: {
-                dots: true, // exibe os pontos a baixo
+                dots: false, // exibe os pontos a baixo
                 infinite: true, // sempre volta ao inicio
                 slidesToShow: 1, // qtdd de slides que serao exibidos
                 slidesToScroll: 1, // qtdd de slides que serao movidos para o lado
